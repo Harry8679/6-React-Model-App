1782462e4677db3e9b1fdd4f7a372d64cbd53bbe
Harry MacCode <emarh.harry.code@gmail.com>
2024-01-18 11:23:11 +0100

    Modal - CSS and Logic

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import "./App.scss";
 import Modal from "./components/modal/Modal";
 import Emarh from './assets/8888.jpg';
+import { useState } from "react";
 
 const App = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClickModal = () => {
+    setShowModal(true);
+  }
+
+  const closeModal = (e) => {
+    if (e.target.classList.contains('cm')) {
+      // console.log('Happens close modal');
+      setShowModal(false);
+    }
+  }
   return (
-    <section className="intro-section">
-      <div className="intro">
-        <img src={Emarh} alt="Emarh" />
-        <h1>Let's <span>Create a Modal</span></h1>
-        <p>Click the Button below</p>
-        <button>OPEN MODAL</button>
-      </div>
-    </section>
+    <>
+      <section className="intro-section --100vh --flex-center">
+        <div className="intro --center-all">
+          <img src={Emarh} alt="Emarh" />
+          <h1 className="--text-light --text-md">Let's <span>Create a Modal</span></h1>
+          <p className="--text-light --mb2">Click the Button below</p>
+          <button className="--btn --btn-secondary" onClick={handleClickModal}>OPEN MODAL</button>
+        </div>
+      </section>
+      {showModal && <Modal onCloseModal={closeModal} />}
+    </>
   );
 }
 
--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,29 +1,60 @@
 import React from 'react';
 import './Modal.scss';
-import { IoIosCloseCircleOutline, IoMdInformationCircleOutline } from "react-icons/io";
+import { IoMdInformationCircleOutline } from "react-icons/io";
+import { IoClose } from "react-icons/io5";
 
 
-const Modal = () => {
+
+const Modal = ({ onCloseModal }) => {
   return (
-    <section className='modal-section'>
-        <div className="modal">
-            <div className="modal-content">
-                <IoIosCloseCircleOutline color='red' size={16} className='close-icon' />
-            </div>
-            <div className="modal-head">
-                <IoMdInformationCircleOutline color='orangered' size={18} />
-                <h3>Do you want to continue ?</h3>
-            </div>
-            <div className="modal-body">
+    <section className="modal-section --100vh cm" onClick={onCloseModal}>
+      <div className="modal --flex-center">
+        <div className="modal-content --bg-light --p --card">
+          <IoClose
+            color="red"
+            size={16}
+            className="close-icon cm"
+            onClick={onCloseModal}
+          />
+
+          <div className="modal-head --flex-start">
+            <IoMdInformationCircleOutline color="orangered" size={18} />
+            <h3 className="--text-p --ml">Do you want to continue?</h3>
+          </div>
+          <div className="modal-body">
+            <p className="--text-sm --mb">
                 Among the children of men, there is only one Lord who on Earth where we are has served you with all his heart.
                 Name Above All Name, Worthy of our Praises, My heart will sing Our Great is Our God.
-            </div>
-            <div className="modal-footer">
-                <button>Cancel</button>
-                <button>Proceed</button>
-            </div>
+            </p>
+          </div>
+          <div className="modal-footer --flex-end">
+            <button className="--btn cm" onClick={onCloseModal}>
+              Cancel
+            </button>
+            <button className="--btn --btn-primary">Proceed</button>
+          </div>
         </div>
+      </div>
     </section>
+    // <section className='modal-section --100vh'>
+    //     <div className="modal --flex-center">
+    //         <div className="modal-content --bg-light --p --card">
+    //             <IoClose color='red' size={16} className='close-icon' />
+    //         </div>
+    //         <div className="modal-head">
+    //             <IoMdInformationCircleOutline color='orangered' size={18} />
+    //             <h3>Do you want to continue ?</h3>
+    //         </div>
+    //         <div className="modal-body">
+    //             Among the children of men, there is only one Lord who on Earth where we are has served you with all his heart.
+    //             Name Above All Name, Worthy of our Praises, My heart will sing Our Great is Our God.
+    //         </div>
+    //         <div className="modal-footer">
+    //             <button>Cancel</button>
+    //             <button>Proceed</button>
+    //         </div>
+    //     </div>
+    // </section>
   )
 }
 
